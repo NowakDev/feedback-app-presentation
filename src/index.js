507a1376304b66562f8d8f1bc1bf8b8c43cd99c9
wlejko <wojciechs.lejko@gmail.com>
2020-05-21 11:16:55 +0200

    screens

--- src/index.js
+++ src/index.js
@@ -1,17 +1,257 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import {
+  Appear,
+  Box,
+  CodePane,
+  CodeSpan,
+  Deck,
+  FlexBox,
+  FullScreen,
+  Grid,
+  Heading,
+  Image,
+  ListItem,
+  Markdown,
+  Notes,
+  OrderedList,
+  Progress,
+  Slide,
+  SpectacleLogo,
+  Stepper,
+  Text,
+  UnorderedList,
+  indentNormalizer,
+} from "spectacle";
+
+// SPECTACLE_CLI_THEME_START
+const theme = {
+  fonts: {
+    header: '"Open Sans Condensed", Helvetica, Arial, sans-serif',
+    text: '"Open Sans Condensed", Helvetica, Arial, sans-serif',
+  },
+};
+// SPECTACLE_CLI_THEME_END
+
+// SPECTACLE_CLI_TEMPLATE_START
+const template = () => (
+  <FlexBox
+    justifyContent="space-between"
+    position="absolute"
+    bottom={0}
+    width={1}
+  >
+    <Box padding="0 1em">
+      <FullScreen />
+    </Box>
+    <Box padding="1em">
+      <Progress />
+    </Box>
+  </FlexBox>
+);
+// SPECTACLE_CLI_TEMPLATE_END
+
+const formidableLogo =
+  "https://avatars2.githubusercontent.com/u/5078602?s=280&v=4";
+
+const cppCodeBlock = indentNormalizer(`
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <pthread.h>
+struct thread_data_t
+{
+   int  thread_id;
+   std::string message;
+};
+void *print_thread_message(void *thread_arg)
+{
+   struct thread_data_t *thread_data;
+   thread_data = (struct thread_data_t *) thread_arg;
+   cout << "Thread ID: " << thread_data->thread_id;
+   cout << "Message: " << thread_data->message << endl;
+   pthread_exit(NULL);
+}
+int main()
+{
+  pthread_t threads[NUM_THREADS];
+  struct thread_data_t thread_data[NUM_THREADS];
+  for (int i = 0; i < NUM_THREADS; i++)
+  {
+    auto curried_add = [](int x) -> function<int(int)> { return [=](int y) { return x + y; }; };
+    auto answer = curried_add(i)(5);
+    std::stringstream message;
+    message << "The math result is " << answer << "!";
+    thread_data.thread_id = i;
+    thread_data.message = message.str();
+    int err = pthread_create(&threads, NULL, print_thread_message, (void *)&thread_data[i]);
+    if (err)
+    {
+      exit(-1)
+    }
+  }
+  return 0;
+}`);
+
+const Presentation = () => (
+  <Deck theme={theme} template={template} transitionEffect="fade">
+    <Slide backgroundColor="#f8f9fa">
+      <FlexBox height="100%" flexDirection="column">
+        <Heading margin="0px" color="#ff5a00">
+          Wstęp
+        </Heading>
+        <Text color="black" textAlign="center">
+          Nasza aplikacja służy do zbierania i przetrzymywania feedbacków o
+          pracownikach naszej firmy. Feedbacki są zbierane z firm zewnętrznych
+          jak i naszej firmy.
+        </Text>
+      </FlexBox>
+    </Slide>
+    <Slide backgroundColor="#f8f9fa">
+      <Heading margin="0px" color="#ff5a00">
+        Technologie
+      </Heading>
+      <FlexBox
+        height="100%"
+        flexDirection="row"
+        justifyContent="space-around"
+        alignItems="start"
+      >
+        <div>
+          <Text color="black">Frontend</Text>
+          <UnorderedList>
+            <ListItem color="black">
+              <CodeSpan>ReactJS</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>React hooks</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>Storybook</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>Fabric-UI</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>TypeScript</CodeSpan>
+            </ListItem>
+          </UnorderedList>
+        </div>
+        <div>
+          <Text color="black">Backend</Text>
+          <UnorderedList>
+            <ListItem color="black">
+              <CodeSpan>Nodejs</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>ExpressJS</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>PassportJS</CodeSpan>
+            </ListItem>
+            <ListItem color="black">
+              <CodeSpan>NestJS</CodeSpan>
+            </ListItem>
+          </UnorderedList>
+        </div>
+      </FlexBox>
+    </Slide>
+    <Slide transitionEffect="fade" backgroundColor="#f8f9fa">
+      <Heading margin="0px" color="#ff5a00">
+        Lista kontaktów
+      </Heading>
+
+      <Text>
+        Code Blocks now auto size and scroll when there is an overflow of
+        content! They also auto-wrap longer lines.
+      </Text>
+    </Slide>
+    <Slide>
+      <Heading>Animated Elements</Heading>
+      <OrderedList>
+        <Appear elementNum={0}>
+          <ListItem>Elements can animate in!</ListItem>
+        </Appear>
+        <Appear elementNum={2}>
+          <ListItem>
+            Just identify the order with the prop{" "}
+            <CodeSpan>elementNum</CodeSpan>!
+          </ListItem>
+        </Appear>
+        <Appear elementNum={1}>
+          <ListItem>Out of order</ListItem>
+        </Appear>
+      </OrderedList>
+    </Slide>
+    <Slide>
+      <FlexBox>
+        <Text>These</Text>
+        <Text>Text</Text>
+        <Text color="secondary">Items</Text>
+        <Text fontWeight="bold">Flex</Text>
+      </FlexBox>
+      <Grid gridTemplateColumns="1fr 2fr" gridColumnGap={15}>
+        <Box backgroundColor="primary">
+          <Text color="secondary">Single-size Grid Item</Text>
+        </Box>
+        <Box backgroundColor="secondary">
+          <Text>Double-size Grid Item</Text>
+        </Box>
+      </Grid>
+      <Grid
+        gridTemplateColumns="1fr 1fr 1fr"
+        gridTemplateRows="1fr 1fr 1fr"
+        alignItems="center"
+        justifyContent="center"
+        gridRowGap={1}
+      >
+        {Array(9)
+          .fill("")
+          .map((_, index) => (
+            <FlexBox paddingTop={0} key={`formidable-logo-${index}`} flex={1}>
+              <Image src={formidableLogo} width={100} />
+            </FlexBox>
+          ))}
+      </Grid>
+    </Slide>
+    <Slide>
+      <Markdown>
+        {`
+          # Layout Tables in Markdown
+          | Browser         | Supported | Versions |
+          |-----------------|-----------|----------|
+          | Chrome          | Yes       | Last 2   |
+          | Firefox         | Yes       | Last 2   |
+          | Opera           | Yes       | Last 2   |
+          | Edge (EdgeHTML) | No        |          |
+          | IE 11           | No        |          |
+        `}
+      </Markdown>
+    </Slide>
+    <Markdown containsSlides>
+      {`
+        ### Even write multiple slides in Markdown
+        > Wonderfully formatted quotes
+        1. Even create
+        2. Lists in Markdown
+        - Or Unordered Lists
+        - Too!!
+        Notes: These are notes
+        ---
+        ### This slide was also generated in Markdown!
+        \`\`\`jsx
+        const evenCooler = "is that you can do code in Markdown";
+        // You can even specify the syntax type!
+        \`\`\`
+        ### A slide can have multiple code blocks too.
+        \`\`\`c
+        char[] someString = "Popular languages like C too!";
+        \`\`\`
+        Notes: These are more notes
+      `}
+    </Markdown>
+  </Deck>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Presentation />, document.getElementById("root"));
