a73eb66f528c8452b7ec8d7dbdc0101c04cbf10f
Kamil Nowak <kamil.nowak@billennium.com>
2020-05-22 14:09:59 +0200

    Updates

---
+++ precache-manifest.72b3bda66061f256069e53930d658e6e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d49f55fec980e0bf9ad92e116a51d4",
+    "url": "/feedback-app-presentation/index.html"
+  },
+  {
+    "revision": "4669231ed6dcded590c5",
+    "url": "/feedback-app-presentation/static/css/main.541d2548.chunk.css"
+  },
+  {
+    "revision": "6b322938705cb734946a",
+    "url": "/feedback-app-presentation/static/js/2.d0b067b2.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/feedback-app-presentation/static/js/2.d0b067b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4669231ed6dcded590c5",
+    "url": "/feedback-app-presentation/static/js/main.fc6af164.chunk.js"
+  },
+  {
+    "revision": "9d3abfa0c68171a028a6",
+    "url": "/feedback-app-presentation/static/js/runtime-main.dab3d665.js"
+  },
+  {
+    "revision": "94a02158e28d4ed7f435ff11c20e4aa0",
+    "url": "/feedback-app-presentation/static/media/Billennium-CompressedBold.94a02158.otf"
+  },
+  {
+    "revision": "78c461c83a054eaa494ec8ac79068a51",
+    "url": "/feedback-app-presentation/static/media/Billennium-Regular.78c461c8.otf"
+  },
+  {
+    "revision": "4525d50a25ff7f2054ec16b5eeca749c",
+    "url": "/feedback-app-presentation/static/media/Billennium-SemiBold.4525d50a.otf"
+  },
+  {
+    "revision": "a9d72e17f9b7372e12ea8ab5afe8ff65",
+    "url": "/feedback-app-presentation/static/media/Sygnatura.a9d72e17.svg"
+  },
+  {
+    "revision": "a35bd591aa2fa6ced2a18ed223aa15f4",
+    "url": "/feedback-app-presentation/static/media/addInvitation.a35bd591.gif"
+  },
+  {
+    "revision": "d2d5b091e96cbd4e8d1173e918ae9a0a",
+    "url": "/feedback-app-presentation/static/media/contactFormAddContact.d2d5b091.gif"
+  },
+  {
+    "revision": "1f53a9aa6f7dac6c41ffe61a610eb5f2",
+    "url": "/feedback-app-presentation/static/media/contactList.1f53a9aa.png"
+  },
+  {
+    "revision": "9896c8beacede41b6b3b2454007de4c1",
+    "url": "/feedback-app-presentation/static/media/contactListSuccess.9896c8be.png"
+  },
+  {
+    "revision": "1b28da423e1c5e088f23527e117bd5a5",
+    "url": "/feedback-app-presentation/static/media/employeeList.1b28da42.png"
+  },
+  {
+    "revision": "594b55887d5b930058137009cf7016ab",
+    "url": "/feedback-app-presentation/static/media/evaluationPage-mobile-2.594b5588.png"
+  },
+  {
+    "revision": "a9500a2a97a703589491fc9c53bb3a69",
+    "url": "/feedback-app-presentation/static/media/evaluationPage-tablet-2.a9500a2a.png"
+  },
+  {
+    "revision": "d2dff4381eb920ad4e26c6c841fa4f96",
+    "url": "/feedback-app-presentation/static/media/evaluationPage.d2dff438.png"
+  },
+  {
+    "revision": "21cafbbe6470f98ccc4be13162ac475d",
+    "url": "/feedback-app-presentation/static/media/feedback-app-logo2.21cafbbe.svg"
+  },
+  {
+    "revision": "943d915e46c7c0b0024f7fbbf9321878",
+    "url": "/feedback-app-presentation/static/media/feedbackList.943d915e.gif"
+  },
+  {
+    "revision": "37e8c347e361f43e082ecbc4d4231258",
+    "url": "/feedback-app-presentation/static/media/invitationEmailPreview.37e8c347.png"
+  },
+  {
+    "revision": "c9d5545dc389e31a8c5da4ba3cea91c0",
+    "url": "/feedback-app-presentation/static/media/linkToPanel.c9d5545d.gif"
+  },
+  {
+    "revision": "1f3850d55d0ce698aa43793244b67393",
+    "url": "/feedback-app-presentation/static/media/mobileEvaluation.1f3850d5.png"
+  },
+  {
+    "revision": "eef721b5cd674035cde1edd87af5e761",
+    "url": "/feedback-app-presentation/static/media/openResponsePanel.eef721b5.gif"
+  },
+  {
+    "revision": "f6abbaea5710b0245914ae056962e794",
+    "url": "/feedback-app-presentation/static/media/openResponsePanelList.f6abbaea.gif"
+  },
+  {
+    "revision": "4d9eb3548fbe061d095e5a42ad69975d",
+    "url": "/feedback-app-presentation/static/media/rightPanel.4d9eb354.png"
+  },
+  {
+    "revision": "a6029b48baf6d0dc866b0f9ef1aaa38a",
+    "url": "/feedback-app-presentation/static/media/statuses.a6029b48.png"
+  },
+  {
+    "revision": "961b990bba9b600bc27e15ae1d97ce02",
+    "url": "/feedback-app-presentation/static/media/tableEvaluationPage.961b990b.jpg"
+  },
+  {
+    "revision": "c495ca6bf9d09fb6051411e2e4c6cbb1",
+    "url": "/feedback-app-presentation/static/media/thankYouPage.c495ca6b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/feedback-app-presentation/precache-manifest.8d0a0606589734e6770815f63db67b21.js"
+  "/feedback-app-presentation/precache-manifest.72b3bda66061f256069e53930d658e6e.js"
 );
 
 self.addEventListener('message', (event) => {
